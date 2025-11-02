43042917bf0eeff93a2d156e8a5de26d8d3a5711
LM <wycalxy@outlook.com>
2025-11-02 14:53:40 +0800

    计算交易金额

---
+++ c4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int a=0,b=0;
+	printf("物品金额:\n");
+	scanf("%d",&a);
+	printf("支付金额:\n");
+	scanf("%d",&b);
+	if (a>=b)
+	{
+	printf("您的钱不够\n");	
+	 } 
+	 else {
+	 	printf("找您%d\n",b-a);
+	 	
+	 }
+	
+	return 0; 
+}
