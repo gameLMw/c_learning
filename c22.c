4c1b45cb2bc373224e86ffcc3b51632a6f6e0acb
LM <wycalxy@outlook.com>
2025-11-03 21:55:47 +0800

    函数学习

---
+++ c22.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int max(int a,int b);//声明 ,函数原型 
+
+int main()
+{
+	int a,b,c;
+	a=5;
+	b=6;
+	c=max(10,12);
+	c=max(a,b);
+	c=max(c,23);
+	printf("%d\n",max(a,b));
+	return 0;
+}
+
+int max(int a,int b)//函数定义 
+{
+	int ret;
+	if(a>b){
+		ret=a;
+	}else{
+		ret=b;
+	}
+	return ret;
+}
