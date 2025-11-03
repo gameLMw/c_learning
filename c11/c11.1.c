2534510ece79091b593cd43e2eb2c1606ce50ebd
LM <wycalxy@outlook.com>
2025-11-03 22:17:41 +0800

    猜数

---
+++ c11/c11.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	srand(time(0));
+	int a =rand()%100+1;
+	int b=0,c=0;
+	printf("请猜一个1到100之间的数:");
+	do
+	{
+		scanf("%d",&b) ;
+		c++;
+		if(b>a){
+		printf("太大了\n");}
+		else{
+		printf("太小了\n") ;}
+	}while(b!=a);
+	printf("你用了%d次\n",c);
+	return 0;
+}
---
+++ c11/c11.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=59,b;
+	scanf("%d",&b);
+	while(b>59)
+	{
+		printf("太大了");
+		scanf("%d",&b); 
+	
+	   while(b<59)
+	   {
+		printf("太小了");
+		scanf("%d",&b); 
+	   }
+  }
+	printf("答对了");
+	return 0;
+}
+/*输出一个随机整数 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	srand(time(0));
+	int a=rand();
+	printf("%d",a);
+	return 0;
+}*/
