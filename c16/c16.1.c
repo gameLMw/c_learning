3d9e5b60d4be26b098ea8bf007e61a5c0dd5637d
LM <wycalxy@outlook.com>
2025-11-03 22:15:59 +0800

    找出一种硬币组合（1角、2角、5角），使得总金额恰好等于 x 元

---
+++ c16/c16.1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int one, two, five;
+    
+    //printf("请输入金额（元）: ");
+    //scanf("%d", &x);
+    x=2; 
+    int count = 0;
+    
+    for(one = 0; one <= x * 10; one++)
+    {
+        for(two = 0; two <= x * 10 / 2; two++)
+        {
+            for(five = 0; five <= x * 10 / 5; five++)
+            {
+                if(one + two * 2 + five * 5 == x * 10)
+                {
+                    printf("可以用%d个1角%d个2角%d个5角得到%d元\n", one, two, five, x);
+                    count++;
+                }
+            }
+        }
+    }
+    
+    printf("共有%d种组合方式\n", count);
+    
+    return 0;
+}
