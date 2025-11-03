58aa2264f7dda44b90f3cc35279051398bd5569b
LM <wycalxy@outlook.com>
2025-11-03 22:09:20 +0800

    指针学习

---
+++ c28.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int a[2] = {0};
+    int *b;          
+    int *p, *i;
+    b = a;          
+    p = &a[0];
+    i = &a[1];
+    printf("%p, %p, %p\n", (void*)b, (void*)p, (void*)i);
+    return 0;
+}
