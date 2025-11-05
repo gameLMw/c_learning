3997422be979a3469d15adaa8052ed7539799da3
LM <wycalxy@outlook.com>
2025-11-05 22:33:13 +0800

    strcat函数

---
+++ c33.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char ar[30] = {" asda sdas"};
+    char arr[30]={" qwe dgsg"};
+    strcat(ar,arr);
+    printf("%s\n",ar);
+    printf("%s\n",arr);
+    strcat(arr,ar);
+    printf("%s\n",arr);
+    printf("%s\n",ar);
+    return 0;
+}
+//sizeof(dest) 必须 ≥ strlen(dest) + strlen(src) + 1
