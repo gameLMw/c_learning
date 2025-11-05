1f10336a90b7164364d77e7ec52f72105504949c
LM <wycalxy@outlook.com>
2025-11-05 22:34:15 +0800

    strcpy函数

---
+++ c34.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h> 
+int main (void)
+{
+    char str1[50];
+    strcpy(str1, "Hello, ");
+    strcat(str1, "World!");
+    printf("%s\n", str1);
+    return 0;
+}
