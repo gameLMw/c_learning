abd5c437c226f289a3511a5665d7cdcaed643a9b
LM <wycalxy@outlook.com>
2025-11-12 23:07:05 +0800

    循环

---
+++ c42.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int arr[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    for (int i = 0; i < 26; i++)
+    {
+        printf("%c ", arr[i]);
+    }
+    return 0;
+}
