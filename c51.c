e72625014da0ed68380655d5d108822b17af982d
LM <wycalxy@outlook.com>
2025-11-23 17:19:37 +0800

    无while

---
+++ c51.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    return 0;
+}
